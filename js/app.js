29b16c49bd37465d82ecbfa964687143ec31635d
haithamassoli <haitham.b.assoli@gmail.com>
2021-10-22 14:52:35 +0300

    adding animation and countdown timer

--- js/app.js
+++ js/app.js
@@ -22,29 +22,56 @@ function next() {
       que_text.innerHTML = data[counter].question;
 
       for (i = 0; i < 4; i++) {
+        const input = document.createElement("input");
         const label = document.createElement("label");
+        input.setAttribute("type", "radio");
+        input.setAttribute("class", "inputDel");
+        input.setAttribute("id", `number${[i]}`);
+        input.setAttribute("name", `Web Developer Shortcuts`);
         label.setAttribute("class", "option");
+        label.setAttribute("for", `number${[i]}`);
+        option_list.appendChild(input);
         option_list.appendChild(label);
         label.innerHTML = data[counter].options[i];
       }
     });
 }
 next();
-function labelText() {}
 
+function timer_line(sec) {
+  line_left = setInterval(function () {
+    time_line.style.width = sec + "px";
+    if (sec >= 548) clearInterval(line_left);
+    sec += 4.575;
+  }, 125);
+}
+function timer_sec(sec) {
+  timer_left = setInterval(function () {
+    timeCount.innerHTML = sec;
+    if (timeCount.innerHTML <= 9) timeCount.innerHTML = `0${sec}`;
+    if (timeCount.innerHTML == 0) {
+      clearInterval(timer_left);
+      timeCount.innerHTML = "time out";
+    }
+    sec--;
+  }, 1000);
+}
 start_btn.addEventListener("click", () => {
   quiz_box.classList.add("activeQuiz");
   next_btn.style.opacity = 1;
   next_btn.style.pointerEvents = "painted";
+  timer_sec(15);
+  timer_line(0);
 });
 
 next_btn.addEventListener("click", () => {
-  const labelText = document.querySelectorAll(".option");
-  que_text.innerHTML = "";
-  labelText.forEach((e) => {
-    console.log(e.innerHTML);
-    e.remove();
-  });
-  counter++;
-  next();
+  if (counter < 4) {
+    const labelText = document.querySelectorAll(".option");
+    que_text.innerHTML = "";
+    labelText.forEach((e) => e.remove());
+    counter++;
+    next();
+  } else console.log("helllo");
+  timer_sec(15);
+  timer_line(0);
 });
